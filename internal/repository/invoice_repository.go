5b199a8c5f9f1f3bc2ddde69a055efeb26544f9c
Daniel Sângelo <danielsangelo@hotmail.com>
2025-04-08 20:31:23 -0300

    fix: insert info when inserting invoice in db

--- internal/repository/invoice_repository.go
+++ internal/repository/invoice_repository.go
@@ -15,31 +15,37 @@ func NewInvoiceRepository(db *sql.DB) *InvoiceRepository {
 }
 
 func (r *InvoiceRepository) Save(invoice *domain.Invoice) error {
-	_, err := r.db.Exec(`
-	INSERT INTO invoices
-	(
-		id,
-		account_id,
-		amount,
-		status,
-		description,
-		created_at,
-		updated_at
+	_, err := r.db.Exec(
+		`INSERT INTO invoices
+		(
+			id,
+			account_id,
+			amount,
+			status,
+			description,
+			payment_type,
+			card_last_digits,
+			created_at,
+			updated_at
+		)
+		VALUES
+		(
+			$1,
+			$2,
+			$3,
+			$4,
+			$5,
+			$6,
+			$7,
+			$8,
+			$9
+		)`,
+		invoice.ID, invoice.AccountID, invoice.Amount, invoice.Status, invoice.Description, invoice.PaymentType, invoice.CardLastDigits, invoice.CreatedAt, invoice.UpdatedAt,
 	)
-	VALUES
-	(
-		$1,
-		$2,
-		$3,
-		$4,
-		$5,
-		$6,
-		$7
-	)`, invoice.ID, invoice.AccountID, invoice.Amount, invoice.Status, invoice.Description, invoice.CreatedAt, invoice.UpdatedAt)
-
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
 
